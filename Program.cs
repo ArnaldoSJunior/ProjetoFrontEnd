87e887d07b844912c2a467c94fa42238fc90ef70
ArnaldoSJunior <arnaldojuniorads@gmail.com>
2024-11-19 19:16:13 -0300

    nova entidade

--- Program.cs
+++ Program.cs
@@ -2,41 +2,32 @@ using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
-
-builder.Services.AddDbContext<AppDataBase>(options =>
-    options.UseMySQL(builder.Configuration.GetConnectionString("DefaultConnection"))
-);
-
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext<AppDataBase>();
 
-
-builder.Services.AddCors(options =>
-{
-    options.AddPolicy("AllowLocalhost", policy =>
-    {
-        policy.WithOrigins("http://localhost:3000")  // Permite acesso do frontend em http://localhost:3000
-              .AllowAnyHeader()
-              .AllowAnyMethod();
-    });
-});
+builder.Services.AddCors();
 
 var app = builder.Build();
 
+app.UseCors(policy => policy
+    .AllowAnyOrigin()
+    .AllowAnyHeader()
+    .AllowAnyMethod()
+);
 
 app.UseSwagger();
 app.UseSwaggerUI();
 
-
 app.MapGet("/", () => "API");
 
-
-ConfigurarRotasCargo.MapRotas(app);
-ConfigurarRotasFuncionario.MapRotas(app);
-ConfigurarRotasBeneficio.MapRotas(app);
-ConfigurarRotasDepartamento.MapRotas(app);
-ConfigurarHistoricoSalario.MapRotas(app);
-
-app.UseCors("AllowLocalhost");
+app.MapRotas();
+app.MapContratosApi();
+app.MapDepartamentoApi();
+app.MapBeneficiosApi();
+app.MapFeriasApi();
+app.MapFuncionarioApi();
+app.MapHistoricoSalarioApi();
+app.MapPontoApi();
 
 app.Run();
